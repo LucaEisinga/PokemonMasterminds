c468c322328446f81e6c361c5d6bb045214d3d32
peter <72741411+PeterB2003@users.noreply.github.com>
2023-06-21 23:26:29 +0200

    simpler code for debuging
    simpler code for debuging

--- PokemonMasterminds/Model/Questions/WhoIsThatPokemon.cs
+++ PokemonMasterminds/Model/Questions/WhoIsThatPokemon.cs
@@ -12,39 +12,24 @@ namespace PokemonMasterminds.Model.Questions
 
                 Random random = new Random();
                 List<Pokemon> fetchedPokemon = new List<Pokemon>();
-                List<Task<Pokemon>> fetchTasks = new List<Task<Pokemon>>();
 
                 for (int i = 0; i < numberOfOptions; i++)
                 {
-                    Task<Pokemon> fetchTask = api.FetchPokemonData(random.Next(x, y + 1));
-                    fetchTasks.Add(fetchTask);
-                }
+                    Pokemon pokemon = null;
+                    bool isValidPokemon = false;
 
-                // Await the completion of all fetch tasks
-                Pokemon[] fetchedResults = await Task.WhenAll(fetchTasks);
+                    while (!isValidPokemon)
+                    {
+                        int randomNumber = random.Next(x, y + 1);
+                        pokemon = await api.FetchPokemonData(randomNumber);
+                        isValidPokemon = pokemon != null && !fetchedPokemon.Contains(pokemon);
+                    }
 
-                // Process the fetched results
-                foreach (Pokemon pokemon in fetchedResults)
-                {
-                    Answers.Add(new Answer(pokemon.name, Answers.Count == 0));
+                    fetchedPokemon.Add(pokemon);
+                    
+                    Answers.Add(new Answer(pokemon.name, i == 0));
                 }
             }
-        }
-
-        private static async Task<Pokemon> FetchRandomPokemon(Api api, Random random, int x, int y, List<Pokemon> fetchedPokemon)
-        {
-            Pokemon pokemon = null;
-            bool isValidPokemon = false;
-
-            while (!isValidPokemon)
-            {
-                int randomNumber = random.Next(x, y + 1);
-                pokemon = await api.FetchPokemonData(randomNumber);
-                isValidPokemon = pokemon != null && !fetchedPokemon.Contains(pokemon);
-            }
-
-            fetchedPokemon.Add(pokemon);
-            return pokemon;
         }
     }
 }
